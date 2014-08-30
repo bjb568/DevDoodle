b53f891e0bb5d0494977c42fb7df2773dc0f6dd5
bjb568 <brianjblair@icloud.com>
2014-08-30 18:24:55 -0400

    Fixed #56 and better inline post handling

--- a/main.js
+++ a/main.js
@@ -17,11 +17,14 @@ function html(input) {
 	return input.toString().replaceAll(['&', '<', '"'], ['&amp;', '&lt;', '&quot;']);
 };
 function markdown(src) {
-	var h = '';
+	var h = '',
+		i = 0;
 	function inlineEscape(s) {
 		return html(s)
 			.replace(/!\[([^\]]*)]\(([^(]+)\)/g, '<img alt="$1" src="$2">')
 			.replace(/\[([^\]]+)]\(([^(]+)\)/g, '$1'.link('$2'))
+			.replace(/([^"])(https?:\/\/([^\s"]+))/g, '$1$3'.link('$1$2'))
+			.replace(/^(https?:\/\/([^\s"]+))/g, '$2'.link('$1'))
 			.replace(/`([^`]+)`/g, '<code>$1</code>')
 			.replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>')
 			.replace(/\*([^*]+)\*/g, '<em>$1</em>');
@@ -37,16 +40,14 @@ function markdown(src) {
 		if (b.match(/\n[1-9]\d*\. /)) R = [(/\n[1-9]\d*\. /), '<ol><li>', '</li></ol>'];
 		if (b.match(/\n[1-9]\d*\) /)) R = [(/\n[1-9]\d*\) /), '<ol><li>', '</li></ol>'];
 		f = b[0];
-		h +=
-			R ? R[1] + ('\n' + b)
-				.split(R[0])
-				.slice(1)
-				.map(R[3] ? html : inlineEscape)
-				.join(R[3] || '</li>\n<li>') + R[2]:
-			f == '#' ? '<h' + (f = b.indexOf(' ')) + '>' + inlineEscape(b.slice(f + 1)) + '</h' + f + '>':
-			f == '<' ? b:
-			'<p>' + inlineEscape(b) + '</p>';
+		if (R) h += R[1] + ('\n' + b).split(R[0]).slice(1).map(R[3] ? html : inlineEscape).join(R[3] || '</li>\n<li>') + R[2];
+		else if (f == '#') h += '<h' + Math.min(6, f = b.indexOf(' ')) + '>' + inlineEscape(b.slice(f + 1)) + '</h' + Math.min(6, f) + '>';
+		else {
+			h += '<p>' + inlineEscape(b) + '</p>';
+			i++;
+		}
 	});
+	if (i == 1) return inlineEscape(src);
 	return h;
 };
 
