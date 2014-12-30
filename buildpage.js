f4c190b56f63e084fd94d05fefee49554a4a6d66
bjb568 <brianjblair@icloud.com>
2014-12-30 15:51:28 -0500

    auto footerOff with clean

--- buildpage.js
+++ buildpage.js
@@ -73,6 +73,7 @@ function respondPage(title, user, req, res, callback, header, status) {
 	delete header.user;
 	delete header.nonotif;
 	delete header.clean;
+	if (clean) inhead += '<script>var footerOff = true;</script>';
 	if (!header['Content-Type']) header['Content-Type'] = 'application/xhtml+xml';
 	if (!header['Cache-Control']) header['Cache-Control'] = 'no-cache';
 	if (user) {
