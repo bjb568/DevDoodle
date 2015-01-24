eb8b9c8156bf63a03afb45b2986ae6d09c34675a
bjb568 <brianjblair@icloud.com>
2015-01-24 14:52:53 -0500

    Fix /chat/ page minHeight()

--- http/a/main.js
+++ http/a/main.js
@@ -246,12 +246,11 @@ function minHeight() {
 		if (sidebar) sidebar.style.height = '';
 	} else {
 		mainContentEl.style.height = '';
-		if (navigator.userAgent.indexOf('Mobile') == -1) mainContentEl.style.minHeight = innerHeight - (footerOff ? -4 : footer) - mainContentEl.getBoundingClientRect().top + document.body.getBoundingClientRect().top - (innerWidth < 1500 ? 6 : 12) - mainBottomPad + 'px';
+		if (navigator.userAgent.indexOf('Mobile') == -1) mainContentEl.style.minHeight = Math.max(0, innerHeight - (footerOff ? -4 : footer) - mainContentEl.getBoundingClientRect().top + document.body.getBoundingClientRect().top - (innerWidth < 1500 ? 6 : 12) - mainBottomPad) + 'px';
 		else mainContentEl.style.minHeight = '';
 		if (sidebar) {
 			if (innerWidth >= 1500) sidebar.style.height = mainContentEl.style.minHeight;
 			else sidebar.style.height = '';
-			if (mainContentEl.style.minHeight != innerHeight - footer - mainContentEl.getBoundingClientRect().top + document.body.getBoundingClientRect().top - (innerWidth < 1500 ? 6 : 12) - mainBottomPad + 'px') minHeight();
 		}
 	}
 }
