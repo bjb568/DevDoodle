a62fc688c168594508dc2239dc1eba06ed19ae49
bjb568 <brianjblair@icloud.com>
2015-10-29 21:05:08 -0400

    Fix mail confirm client-side JS

---
+++ http/login/mail-confirm.js
@@ -0,0 +1,7 @@
+document.getElementById('resendform').onsubmit = function(e) {
+	e.preventDefault();
+	request('/api/login/resend', function(res) {
+		document.getElementById('mailinput').disabled = document.getElementById('submit').disabled = true;
+		alert(res);
+	}, 'name=' + encodeURIComponent(document.getElementById('userinput').value) + '&pass=' + encodeURIComponent(document.getElementById('passinput').value) + '&mail=' + encodeURIComponent(document.getElementById('mailinput').value));
+};
