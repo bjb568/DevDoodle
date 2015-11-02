49a5b1de1e057a0d077105a891af447186e91214
bjb568 <brianjblair@icloud.com>
2015-11-01 19:23:35 -0500

    Fix password recovery JS

--- http/login/recover.js
+++ http/login/recover.js
@@ -1,47 +1,42 @@
-var uname = document.getElementById('uname'),
+var sendmail = document.getElementById('sendmail'),
+	reset = document.getElementById('reset'),
+	uname = document.getElementById('uname'),
 	mail = document.getElementById('mail'),
 	submit1 = document.getElementById('submit1'),
 	part2 = document.getElementById('part2'),
 	code = document.getElementById('code'),
 	pass = document.getElementById('pass'),
 	passc = document.getElementById('passc'),
-	passStrength = document.getElementById('pass-strength'),
+	passBar = document.getElementById('pass-bar'),
+	passBarOuter = document.getElementById('pass-bar-outer'),
+	passBad = document.getElementById('pass-bad'),
 	passMatch = document.getElementById('pass-match'),
 	submit2 = document.getElementById('submit2');
-document.getElementById('sendmail').onsubmit = function(e) {
+reset.onclick = function() {
+	if (uname.disabled) location.reload();
+	else uname.focus();
+};
+sendmail.oninput = function() {
+	submit1.disabled = !uname.value || !mail.value;
+};
+sendmail.onsubmit = function(e) {
 	e.preventDefault();
+	if (!uname.value || !mail.value) return;
 	request('/api/login/recover', function(res) {
 		uname.disabled = mail.disabled = submit1.disabled = true;
+		part2.hidden = false;
 		part2.classList.remove('hide');
 		code.focus();
 	}, 'user=' + encodeURIComponent(uname.value) + '&mail=' + encodeURIComponent(mail.value));
 };
 code.oninput = pass.oninput = passc.oninput = function() {
 	var fail = false;
-	var uniqueChars = [];
-	for (var i = 0; i < pass.value.length; i++) {
-		if (uniqueChars.indexOf(pass.value[i]) == -1) uniqueChars.push(pass.value[i]);
-	}
-	var matches = pass.value.match(/\d+|[a-z]{5,}|[A-z]{6,}/g) || [],
-		penalty = 0;
-	for (var i = 0; i < matches.length; i++) {
-		penalty += matches[i].length;
-	}
-	var strength = uniqueChars.length - Math.sqrt(penalty) / 3 + pass.value.length / 10;
-	if (!pass.value) {
-		passStrength.textContent = '';
-		fail = true;
-	} else if (strength < 8) {
-		passStrength.style.color = '#f00';
-		passStrength.textContent = 'Weak password';
-		fail = true;
-	} else if (strength < 16) {
-		passStrength.style.color = '#940';
-		passStrength.textContent = 'Ok password, could be better';
-	} else {
-		passStrength.style.color = '#00f';
-		passStrength.textContent = 'Strong password';
-	}
+	var strength = passStrength(pass.value);
+	passBad.hidden = !(pass.value && strength < 1/4);
+	fail |= strength < 1/4;
+	passBarOuter.style.opacity = pass.value ? '1' : '';
+	passBar.style.width = strength * 100 + '%';
+	passBar.style.background = 'hsl(' + strength * 130 + ', 100%, 50%)';
 	if (!passc.value) {
 		fail = true;
 		passMatch.hidden = true;
